857fd08099b0e0dcf2530448a1398df31d54b8c3
Nadim Mahmud <nadimmahmud@Nadims-Mac-mini.local>
2025-06-03 23:29:02 +0600

    https method added.

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -17,7 +17,7 @@ function Weather() {
     }
 
     try{
-        const url = `http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_APP_ID}&q=${city}`
+        const url = `https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_APP_ID}&q=${city}`
         const response = await fetch(url)
         const data = await response.json()
         console.log(data)
